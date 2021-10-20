11e86418f683eda6ecec406da5d3c51410d200b5
fluttertutorialin <kamal.lakhani56@gmail.com>
2021-10-20 06:40:17 -0500

    webview add

--- lib/ui/page/web_view_page.dart
+++ lib/ui/page/web_view_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../widget/venues_item_view.dart';
 import '../../controller/import_collection_controller.dart';
-import '../../resource/app_value.dart';
 import '../../resource/app_color.dart';
 
 class WebViewPage extends GetWidget<WebViewController> {
